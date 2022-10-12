41f15e4d7bcef2e77688dc22d75a4cc126b10696
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-11 20:24:55 -0400

    added comments

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,12 @@
 const sentence = "hello there from lighthouse labs";
-  
+
+//loop through each character, setting a timer that accumulates for each element
 for (let i = 0, j = 100; i < sentence.length; i++, j += 100) {
   setTimeout(() => {
     process.stdout.write(sentence[i]);
+    //if its the last character output newline
     if (i === sentence.length - 1) {
       process.stdout.write('\n');
     }
-  }, j);
+  }, j);//j adds 100ms for each character
 }
